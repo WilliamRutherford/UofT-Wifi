16fa0a01236359c26ef6c655214d96b37f95499c
WilliamRutherford <william@williamrutherford.org>
2016-10-08 21:14:09 -0400

    Added arg functionality

--- UofT-Wifi.sh
+++ UofT-Wifi.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
 DIR=UofTWirelessInfo
-FILE="popUp.php?name=0010"
+FILE="popUp.php?name=00"
+
+#Values of $1:
+#1   AA
+#2   AC
+#3   BW
+#4   HW
+#5   MW
+#6   SL
+#7   SY
+#8   SW
+#9   SRC
+#10  IC
+#11  CCC
+#12  RC
+#13  PO
+#14  MLH
+#15  SR1
+#16  SR2
+#17  JFH
+#18  NR
+
+
+if [ "$#" -eq 0 ]; then
+	echo "no argument"
+fi
+
+if [ $1 -lt 10 ]; then
+	FILE="$FILE""0""$1"
+else
+	FILE=$FILE$1
+fi
 
 if [ ! -d "$DIR" ]; then
 
