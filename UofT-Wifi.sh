d7cce1a6ae1cbb5c258da7ac5bbfda6ecebe677e
WilliamRutherford <william@williamrutherford.org>
2016-10-09 01:15:27 -0400

    added -o flag

--- UofT-Wifi.sh
+++ UofT-Wifi.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
+#DIR represents the directory where all request files are saved to.
+DIR=UTWI
 
-DIR=UofTWirelessInfo
+
+#WARNING!!! DO NOT MODIFY FILE.
 FILE="popUp.php?name=00"
+
+build_to_num(){
+
+	echo "not implemented"	
+
+}
+
+file_to_build(){
+
+	get_file
+
+	cat  "$FILE" | grep -o '<CENTER>.*</CENTER>' | sed 's/<CENTER>//' | sed 's:</CENTER>::' | tr -d "\n"
+
+}
+
+file_to_AP(){
+
+	get_file	
+
+	cat  "$FILE" | grep -o ':.*<p' | sed 's/.*: //' | sed 's/<P.*//' | tr -d "\n"
+}
+
+get_file(){
+
+
+	if [ ! -d "$DIR" ]; then
+
+		mkdir "$DIR"
+	fi
+
+	cd "$DIR"
+
+	wget -qN  "http://utsc.utoronto.ca/webapps/wirelessmap/$FILE"
+
+}
 
 #Values of $1:
 #1   AA
@@ -25,24 +63,53 @@ FILE="popUp.php?name=00"
 
 
 if [ "$#" -eq 0 ]; then
-	echo "no argument"
+	echo "no arguments"
+
+elif [ "$#" -eq 1 ]; then
+	if [ "$1" = "-f" ]; then
+		#returns fastest connection
+		echo "unimplemented"
+	elif [ "$1" = "-a" ]; then
+		#returns all connections
+		for i in {1..18}
+		do
+			./UofT-Wifi.sh -c "$i"
+		done
+	elif [ "$1" = "-h" ]; then
+		#returns help dialogue
+		echo "unimplemented"
+	fi
+
+elif [ "$#" -eq 2 ]; then
+	if [ $1 = "-n" ]; then
+	#the case where $2 is a number
+		if  [ $2 -lt 10 ]; then
+			FILE="$FILE""0"
+		fi
+		FILE="$FILE$2"
+
+		get_file
+		
+		file_to_build
+		echo "\n"
+		echo -n 'Average number of connections per AP : '
+		file_to_AP
+		echo "\n"
+	elif [ "$1" = "-o" ]; then
+		
+		if  [ $2 -lt 10 ]; then
+			FILE="$FILE""0"
+		fi
+		FILE="$FILE$2"
+
+		get_file
+		file_to_build 
+		echo -n "     connect/AP:  "
+		file_to_AP
+		echo -e
+
+	elif [ "$1" = "-b" ]; then
+		#the case where $b is a 2 or 3 digit code to represent a building.
+		echo "unimplemented"
+	fi
 fi
-
-if [ $1 -lt 10 ]; then
-	FILE="$FILE""0""$1"
-else
-	FILE=$FILE$1
-fi
-
-if [ ! -d "$DIR" ]; then
-
-	mkdir "$DIR"
-fi
-
-cd "$DIR"
-
-wget -qN  "http://utsc.utoronto.ca/webapps/wirelessmap/$FILE"
-
-cat "$FILE" | grep -o '<CENTER>.*</CENTER>' | sed 's/<CENTER>//' | sed 's:</CENTER>::'
-
-cat "$FILE" | grep -o 'Average number of connections per AP :.*<p' | sed 's/<P.*//'
